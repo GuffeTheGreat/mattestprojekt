547a5147a4449261e46e85954f85c498e57c2e36
DenDerDoofus <kristofferwbc@gmail.com>
2022-12-12 17:21:27 +0100

    1

--- DDU TK - Projekt - Godot/Start_Side.gd
+++ DDU TK - Projekt - Godot/Start_Side.gd
@@ -3,18 +3,16 @@ extends Control
 onready var logic = Button_Logic.new()
 
 func _ready():
-	add_child(logic)
+	add_child(logic) 
 
-func go_to_login():
+func _process(delta):
+	if get_global_mouse_position().x <= 52 and $Side_bar.rect_position.x < 0 and $AnimationPlayer.current_animation != "Show":
+		$AnimationPlayer.play("Show")
+	elif get_global_mouse_position().x > 52 and $Side_bar.rect_position.x > -55 and $AnimationPlayer.current_animation != "Hide":
+		$AnimationPlayer.play("Hide")
+
+func go_to_tab(tab_value):
 	
-	for n in $TabContainer/Log_Ind_valg/HBoxContainer.get_children():
-		logic.hide_anim(n)
-	yield(get_tree().create_timer(0.7),"timeout")
-	$TabContainer.current_tab = 1
-	for n in $TabContainer/Log_Ind/HBoxContainer.get_children():
-		logic.show_anim(n)
-
-func go_back():
 	for n in $TabContainer.get_child($TabContainer.current_tab).get_children():
 		if n.is_class("VBoxContainer"):
 			for h in n.get_children():
@@ -22,10 +20,13 @@ func go_back():
 		else:
 			logic.hide_anim(n)
 	yield(get_tree().create_timer(0.7),"timeout")
-	$TabContainer.current_tab -= 1
+	$TabContainer.current_tab = tab_value
 	for n in $TabContainer.get_child($TabContainer.current_tab).get_children():
 		if n.is_class("VBoxContainer"):
 			for h in n.get_children():
 				logic.show_anim(h)
 		else:
 			logic.show_anim(n)
+
+func go_back():
+	go_to_tab(0)
